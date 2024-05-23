044bf27c7c00cc875c4524c1c532e2faffd6782b
Flying-Pegasus <atulya1804@gmail.com>
2024-05-23 16:13:27 +0530

    "Final Change"

---
+++ script.js
@@ -0,0 +1,27 @@
+const butt = document.querySelector("h1").style;
+const butt1 = document.querySelector("h1");
+let present = "clicked"
+
+const Clicked =()=>{
+  butt.backgroundColor = "whitesmoke";
+  butt.color = "green";
+  butt.boxShadow = "inset 8px 8px 10px rgba(0, 0, 0,0.7)";
+}
+const notClicked =()=>{
+  butt.backgroundColor = "black";
+  butt.color = "white"; 
+  // butt.border = "0.01px solid whitesmoke";
+  butt.boxShadow = "8px 8px 10px rgba(85, 85, 85,0.6)";
+}
+
+
+butt1.addEventListener("click",()=>{
+  if (present==="notClicked"){
+    present= "clicked";
+    Clicked();
+  }
+  else if (present==="clicked"){
+    present= "notClicked";
+    notClicked();
+  }
+});
